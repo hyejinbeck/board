a48622947aef457c5061c15a2e512fb5c8922d0b
hyejin beck <beckhye@gmail.com>
2023-08-23 13:58:06 +0900

    new->create게시물추가후 반영완료

--- posts/urls.py
+++ posts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('<int:id>/', views.detail,name='detail'),
     path('new/', views.new, name='new'), 
-
+    path('create/',views.create, name='create'),
 ]
